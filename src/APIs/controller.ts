010e9bd8e095736518a281eeea663e98077ec78a
Syed Adeel <onnexdevelopment@gmail.com>
2024-09-26 16:02:45 +0500

    feat: ✨ Health endpoint implementation

--- src/APIs/controller.ts
+++ src/APIs/controller.ts
@@ -2,12 +2,25 @@ import { NextFunction, Request, Response } from 'express'
 import httpResponse from '../handlers/httpResponse'
 import responseMessage from '../constant/responseMessage'
 import httpError from '../handlers/errorHandler/httpError'
+import health from '../utils/health'
 
 export default {
     self: (request: Request, response: Response, next: NextFunction) => {
         try {
             // throw new Error('errors')
             httpResponse(response, request, 200, responseMessage.SUCCESS, null)
+        } catch (error) {
+            httpError(next, error, request, 500)
+        }
+    },
+    health: (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const healthData = {
+                application: health.getApplicationHealth(),
+                system: health.getSystemHealth(),
+                timeStamp: Date.now()
+            }
+            httpResponse(response, request, 200, responseMessage.SUCCESS, healthData)
         } catch (error) {
             httpError(next, error, request, 500)
         }
--- src/APIs/router.ts
+++ src/APIs/router.ts
@@ -4,5 +4,6 @@ import controller from './controller'
 const router = Router()
 
 router.route('/self').get(controller.self)
+router.route('/health').get(controller.health)
 
 export default router
