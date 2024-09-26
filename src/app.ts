94fcda1736bd8e00aa1236dfc47d6bcba3a5bdc1
Syed Adeel <onnexdevelopment@gmail.com>
2024-09-26 16:19:50 +0500

    feat: ✨ helmet and cors setup

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,20 @@ import path from 'path'
 import router from './APIs/router'
 import errorHandler from './middlewares/errorHandler'
 import notFound from './handlers/notFound'
+import helmet from 'helmet'
+import cors from 'cors'
 
 const app: Application = express()
 
 //Middlewares
+app.use(helmet())
+app.use(
+    cors({
+        methods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'HEAD', 'PUT', 'PATCH'],
+        origin: ['https://xyz.com'],
+        credentials: true
+    })
+)
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
